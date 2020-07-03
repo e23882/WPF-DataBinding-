5d62e8ce34ee1c0ae70b10480961e1aa0d8bfb9d
Leoyang <leoyang@jepun.com.tw>
2020-07-03 18:35:47 +0800

    first version commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            ViewModel main = new ViewModel();
+            this.DataContext = main;
+        }
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFApp
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        #region Declarations
+        public event PropertyChangedEventHandler PropertyChanged;
+        private bool _IsChecked = true;
+        private string _Content = "Default Text";
+        #endregion
+
+        #region Property
+        public bool IsChecked
+        {
+            get
+            {
+                return _IsChecked;
+            }
+            set
+            {
+                _IsChecked = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Content
+        {
+            get
+            {
+                return _Content;
+            }
+            set
+            {
+                _Content = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Memberfunction
+        public void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
